8d0e16ea5a4159f898549aa9ad627bab4b8bf388
Thomas Sandmann <sandmann.t@gmail.com>
2013-01-11 16:38:38 +0000

    added splitPerturbations function to automatically derive single-factor comparisons from larger ExpressionSets
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gCMAP@72463 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/utility-functions.R
+++ R/utility-functions.R
@@ -927,3 +927,160 @@ eset_instances <- function(instance.matrix,
   })
 }
 
+splitPerturbations <- function( eset, 
+                                control="none",
+                                controlled.factors="none", ## all, none or vector
+                                factor.of.interest="Compound",
+                                ignore.factors=NULL,
+                                cmap.column="cmap",
+                                prefix="^Factor"){
+  
+  ## get annotation information from the phenoData slot
+  pd <- pData( eset )
+  
+  ## remove columns matching the 'ignore.factors' parameter
+  if( !is.null(ignore.factors)){
+    ignore.factors <- sapply(ignore.factors, function( x ){
+      m <- grep( x, grep( prefix, colnames(pd), value=TRUE), value=TRUE )
+      if( length(m) == 0){
+        return(NA)
+      } else {
+        return( m )
+      }
+    })
+    
+    if( all(is.na(ignore.factors))){
+      stop("None of the 'ignore.factors' could be found in the phenoData.")
+    } else {
+      message( sprintf("The following factor(s) will be ignored: %s",
+                       paste(ignore.factors, collapse=", ")))
+      pd <- pd[,!colnames(pd) %in% ignore.factors]
+    }
+  }
+  
+  ## identify experimental factors by the "Factor" prefix
+  factor.all <- grep( prefix, colnames(pd), value=TRUE)
+  
+  ## match / extract the user-specified factors of interest
+  factor.of.interest <- grep( factor.of.interest, factor.all, value=TRUE)
+  if( length( factor.of.interest ) == 0){
+    stop( "Your factor of interest could not be found in the dataset.")
+  } else if( length( factor.of.interest) > 1) {
+    stop( "The 'factor.of.interest' parameter matches multiple annotation columns.")
+  }
+  message(sprintf("Using %s as factor of interest.", factor.of.interest))
+  
+  other.factors <- setdiff( factor.all, factor.of.interest)
+  perturbations <- unique( setdiff( pd[,factor.of.interest], control))
+  
+  if( length( perturbations) == 0){
+    stop( "No perturbations could be found in the 'factor.of.interest' column.")
+  }
+  
+  if( !identical( controlled.factors,"all") & !identical( controlled.factors,"none")){
+    controlled.factors <- sapply(controlled.factors, function( x) { 
+      grep( x, factor.all,value=TRUE)
+    })
+  } else if( controlled.factors == "all"){
+    controlled.factors <- other.factors
+  } else if( controlled.factors == "none"){
+    controlled.factors <- controlled.factors
+  }
+  
+  ## identify control and perturbation instances
+  if( length( other.factors) == 0){
+    control.instances <- pd[pd[, factor.of.interest] == control, factor.of.interest, drop=FALSE ]
+  } else {
+    control.instances <- pd[pd[, factor.of.interest] == control, other.factors,drop=FALSE ]
+  }
+  
+  if( nrow( control.instances) == 0){
+    stop( sprintf("No control instances could be found matching %s in the %s column.", control, factor.of.interest))
+  }
+  
+  perturb.instances <- pd[pd[, factor.of.interest] != control, factor.all, drop=FALSE ]
+  
+  ## iterate over perturbations and compile single-factor experiments
+  all.instances <- lapply( perturbations, function( x ) {
+    ## unique experimental conditions
+    if( length(other.factors) == 0){
+      perturb.unique <- unique(  perturb.instances[ which( perturb.instances[,factor.of.interest] == x),factor.of.interest, drop=FALSE])
+      perturb.all <- perturb.instances[which( perturb.instances[,factor.of.interest] == x), factor.of.interest, drop=FALSE]
+    } else {
+    perturb.unique <- unique(  perturb.instances[ which( perturb.instances[,factor.of.interest] == x),other.factors, drop=FALSE])
+    perturb.all <- perturb.instances[which( perturb.instances[,factor.of.interest] == x), other.factors, drop=FALSE]
+    }
+    
+    ## assign samples to unique experimental conditions
+    matched.samples <- lapply( 1:nrow( perturb.unique), 
+                               function( n ){
+                                 sapply( 1:nrow( perturb.all),
+                                         function( m ){
+                                           all( perturb.all[m,] == perturb.unique[n,])
+                                         })
+                               })
+    
+    matched.samples <- lapply( matched.samples, function(x){ 
+      row.names(perturb.all)[x]
+    })
+    
+    if( any( sapply( matched.samples, length) != 1)){
+      matched.samples <- I(matched.samples)
+    } else {
+      matched.samples <- unlist(matched.samples)
+    }
+    
+    ## identify suitable controls
+    if( identical( controlled.factors,"none")){
+      matched.controls <- lapply( 1:nrow( perturb.unique),function(n){
+        row.names( control.instances )
+      })
+    } else {
+      matched.controls <- lapply( 1:nrow( perturb.unique), 
+                                  function( n ){
+                                    sapply( 1:nrow( control.instances),
+                                            function( m ){
+                                              all( control.instances[m, controlled.factors] == perturb.unique[n,controlled.factors])
+                                            })
+                                  })
+      matched.controls <- lapply( matched.controls, function(x){ 
+        row.names(control.instances)[x]
+      })
+    }
+    
+    if( all( is.na(unlist(matched.controls)))){
+      warning(sprintf("No control samples found for %s", x), call.=FALSE)
+      return( NA)
+    }
+    
+    if( any( sapply( matched.controls, length) != 1)){
+      matched.controls <- I(matched.controls)
+    } else {
+      matched.controls <- unlist(matched.controls)
+    }
+    
+    ## return results as a data.frame
+    data.frame(perturbagen=x, 
+               perturb.unique, 
+               data.frame( matched.samples, stringsAsFactors=FALSE), 
+               data.frame( matched.controls,stringsAsFactors=FALSE))
+  })
+  names( all.instances) <- perturbations
+  
+  instances <- unlist(
+    lapply( all.instances[!is.na(all.instances)], function( x ){
+      lapply( 1:nrow( x ), function( n) {
+        selected.sampes <- c( x[n,"matched.samples"], 
+                              x[n,"matched.controls"])
+        selected.sampes <- unlist( selected.sampes)
+      })
+    }), recursive=FALSE)
+  
+  eset.list <- lapply( instances, function( x ){
+    instance.eset <- eset[, x]
+    pData(instance.eset)[,cmap.column] <- ifelse( pData( instance.eset)[,factor.of.interest] == "none", "control", "perturbation")
+    return( instance.eset)
+  })
+  return( eset.list)
+}
+
